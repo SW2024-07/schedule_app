561350790dcf2fb9ccce56cb51a22089415a52aa
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-11-06 08:42:45 +0000

    config.hosts追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  config.hosts << "ca3f0196a7e6479e8828b331a71ec0a5.vfs.cloud9.us-east-1.amazonaws.com"
 end
