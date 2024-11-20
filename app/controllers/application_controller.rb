4d648f23574e7c97360a366091c6645af0ee406f
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-11-20 06:58:03 +0000

    ヘルパーメソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  
+  private
+  def current_user
+    if session[:login_uid]
+      User.find_by(uid: session[:login_uid])
+    end
+  end
+  
+  def logged_in?
+    current_user.present?
+  end
+  
 end
