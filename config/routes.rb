505f1c7ab539375afd95004bdf485cb1623fc83b
Ubuntu <ubuntu@ip-172-31-54-248.ec2.internal>
2024-11-27 07:53:16 +0000

    Your commit message here

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
   get 'top/main'
   get 'top/login'
   get 'top/logout'
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   get 'login', to: 'top#login'
   post 'login', to: 'top#login'
   delete 'logout', to: 'top#logout'
+  #root to: 'blogs#index'
+  resources :blogs
 
   resources :users, only: [:new, :create, :show] # 新規登録とマイページ
   resources :calendars # 他のリソースも必要に応じて追加
