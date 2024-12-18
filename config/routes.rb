9a09a3a245afedc4ac13b7677b22a0dfaa8d3e51
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-12-18 08:18:28 +0000

    user_calendar機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :blogs
-  get 'blogs/index'
   get 'top/main'
   get 'top/login'
   get 'top/logout'
-  #root 'top#main' # トップページ
   get 'login', to: 'top#login'
   post 'login', to: 'top#login'
   get 'top/logout', to: 'top#logout', as: 'logout'
   root 'top#main'
- #asd
+  
+  get 'mypage', to: 'users#show', as: :mypage
+  
   resources :users, only: [:new, :create, :show] # 新規登録とマイページ
+  resources :user_calendars
 end
