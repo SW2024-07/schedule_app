616136dffdf1410ffd677a10de44e84e2accf7cf
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-12-18 09:06:14 +0000

    ビューの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,9 @@ Rails.application.routes.draw do
   post 'login', to: 'top#login'
   get 'top/logout', to: 'top#logout', as: 'logout'
   root 'top#main'
-<<<<<<< HEAD
-  get 'search', to: 'search#search', as: 'search'
- #asd
-=======
   
   get 'mypage', to: 'users#show', as: :mypage
   
->>>>>>> origin/main
   resources :users, only: [:new, :create, :show] # 新規登録とマイページ
   resources :user_calendars
   resources :user_calendars do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_18_080256) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_18_082043) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,13 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_18_080256) do
     t.datetime "start_time", precision: nil
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
-    t.boolean "is_visible"
   end
 
-  create_table "calendars", force: :cascade do |t|
-    t.string "name"
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "user_calendar_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_calendar_id"], name: "index_events_on_user_calendar_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,6 +41,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_18_080256) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_calendars", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_calendars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +60,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_18_080256) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "user_calendars"
+  add_foreign_key "user_calendars", "users"
 end
